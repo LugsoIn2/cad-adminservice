db8e800a0ec634877083e90332ef537c103f23cf
Timmy <timmy.pfau@netpy.de>
2022-12-20 02:24:22 +0100

    dockerized and add some terraform (in progress)

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,6 +4,9 @@ terraform {
       source = "hashicorp/aws"
       version = "4.38.0"
     }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
   }
 }
 
@@ -14,3 +17,9 @@ provider "aws" {
     secret_key = var.secret_key
 }
 
+provider "kubernetes" {
+  host = var.k8s_host
+  client_certificate     = base64decode(var.k8s_client_certificate)
+  client_key             = base64decode(var.k8s_client_key)
+  cluster_ca_certificate = base64decode(var.k8s_cluster_ca_certificate)
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,3 +5,20 @@ variable "access_key" {
 variable "secret_key" {
   
 }
+
+variable "k8s_client_certificate" {
+
+}
+
+variable "k8s_client_key" {
+  
+}
+
+variable "k8s_cluster_ca_certificate" {
+  
+}
+
+variable "k8s_host" {
+  
+}
+
