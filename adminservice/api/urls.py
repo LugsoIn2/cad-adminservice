6f7eb367d02e3b067cb07ef5a3f787856b771ba7
v1lling <sascha.villing@web.de>
2023-01-03 13:09:09 +0100

    Get tenant details

--- adminservice/api/urls.py
+++ adminservice/api/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('session/', views.session_view, name='api-session'),
     path('whoami/', views.whoami_view, name='api-whoami'),
     path('register/', views.register_view, name='api-whoami'),
-
+    path('mytenant/', views.get_my_tenant, name='api-mytenant'),
 ]
