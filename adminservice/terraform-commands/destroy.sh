37699f50d703039dbf6e080f53251d96503df5e8
v1lling <sascha.villing@web.de>
2023-01-19 17:20:20 +0100

    Add destroy sh

--- adminservice/terraform-commands/destroy.sh
+++ adminservice/terraform-commands/destroy.sh
@@ -1,2 +1,9 @@
-# TODO:
-# get customer Id as input arg and destroy the UUID
+#!/bin/bash
+customer_id=$1
+if [[ -n "$customer_id" ]]; then
+    terraform -chdir=/cad-terraform-all/subsc_enterprise init
+    terraform -chdir=/cad-terraform-all/subsc_enterprise workspace select $customer_id || terraform -chdir=/cad-terraform-all/subsc_enterprise workspace new $customer_id
+    terraform -chdir=/cad-terraform-all/subsc_enterprise destroy -auto-approve
+else
+    echo "argument error"
+fi
